af33defecceee1ab257f0cd05c06f0c128d6c8f7
mrnegmat <mr.negmat@gmail.com>
2023-01-08 13:42:41 +0500

    added ansible files

---
+++ aws/authentication.sh
@@ -0,0 +1,9 @@
+IAM admin user
+export AWS_ACCESS_KEY_ID='AK123'
+export AWS_SECRET_ACCESS_KEY='abc123'
+echo "qeqwew" > ~/.bashrc
+source .bashrc
+apt install python3-pip -y
+pip install boto3
+pip install boto
+
