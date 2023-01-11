252404689f13684760d031e3b69f4cf1dbd840cd
mrnegmat <mr.negmat@gmail.com>
2023-01-11 23:59:28 +0500

    app.py file modified for test

--- dockercompose/app.py
+++ dockercompose/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from DevOps Engineers - We are High Paid Guys! {} times.\n'.format(count)
